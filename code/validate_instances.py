fa9463d352482d8ce4f97ce655ef2d9bfd58036a
meyer <meyer@europa.local>
2016-06-23 19:42:00 -0400

    remove hard-coding of example path

--- code/validate_instances.py
+++ code/validate_instances.py
@@ -5,15 +5,18 @@ from jsonschema import RefResolver, Draft4Validator
 from os.path import join
 
 
-def validate_instance(filename, error_printing):
+def validate_instance(filename, error_printing, path = None):
     schemasPath = os.path.abspath("../json-schemas")
-    path = "../examples"
+    #path = "../examples"
+    if None != path:
+        instance = json.load(open(join(path,filename)))
+    else:
+        instance = json.load(open(filename))
     datasetSchema = json.load(open(join(schemasPath,"dataset_schema.json")))
     instrumentSchema = json.load(open(join(schemasPath,"instrument_schema.json")))
     resolver = RefResolver('file://'+schemasPath+'/'+"dataset_schema.json", datasetSchema) #, base_uri=schemasPath)
     validator = Draft4Validator(datasetSchema, resolver=resolver)
     print("Validating ", filename)
-    instance = json.load(open(join(path,filename)))
 
     if (error_printing == 0):
         errors = sorted(validator.iter_errors(instance), key=lambda e: e.path)
@@ -38,3 +41,4 @@ if __name__ == '__main__':
     else:
         print('format is: validate_instance.py [input file]')
         sys.exit(1)
+
